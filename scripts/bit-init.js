50844d6b5ca996e1612f1bab77886ac3aea87f7c
Ashan Fernando <ashan256@gmail.com>
2023-07-21 00:28:51 +0530

    Update bit-init.js

--- scripts/bit-init.js
+++ scripts/bit-init.js
@@ -2,26 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-function removeSchemeUrl(inputString) {
-  const urlRegex = /(https?:\/\/[^\s]+)/g;
-  return inputString.replace(urlRegex, '",');
-}
-
-function removeComments(jsonc) {
-  return jsonc.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
-}
-
 const run = async (bitConfigUserToken, wsdir) => {
-  const wsDirPath = path.resolve(wsdir);
-  const wsFile = path.join(wsDirPath, "workspace.jsonc");
-  const workspace = fs.readFileSync(wsFile).toString();
-  const workspaceJson = removeComments(removeSchemeUrl(workspace));
-  const workspaceObject = JSON.parse(workspaceJson);
-  const defaultScope = workspaceObject['teambit.workspace/workspace'].defaultScope;
-  const [org, scope] = defaultScope.split(".");
-  process.env.ORG = org;
-  process.env.SCOPE = scope;
-
   execSync("npx @teambit/bvm install", { shell: '/bin/bash' });
   process.env.PATH = `${process.env.HOME}/bin:` + process.env.PATH; // This step may change depending on your CI runner
 
