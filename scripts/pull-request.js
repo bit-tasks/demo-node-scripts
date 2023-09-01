b5d21d30ef4eabf12eb81180fa570161084675db
Ashan Fernando <ashan256@gmail.com>
2023-09-01 17:47:06 +0530

    Update pull-request.js

--- scripts/pull-request.js
+++ scripts/pull-request.js
@@ -1,6 +1,10 @@
 const { execSync } = require("child_process");
 
 const run = (org, scope, lane, wsdir) => {
+  execSync("bit status --strict", { cwd: wsdir, shell: "/bin/bash" });
+  execSync(`bit lane create ${lane}`, { cwd: wsdir, shell: "/bin/bash" });
+  execSync('bit snap -m "CI"', { cwd: wsdir, shell: "/bin/bash" });
+
   try {
     execSync(`bit lane remove ${org}.${scope}/${lane} --remote --silent`, {
       cwd: wsdir,
@@ -11,10 +15,7 @@ const run = (org, scope, lane, wsdir) => {
       `Error while removing bit lane: ${error}. Lane may not exist`
     );
   }
-
-  execSync("bit status --strict", { cwd: wsdir, shell: "/bin/bash" });
-  execSync(`bit lane create ${lane}`, { cwd: wsdir, shell: "/bin/bash" });
-  execSync('bit snap -m "CI"', { cwd: wsdir, shell: "/bin/bash" });
+  
   execSync("bit export", { cwd: wsdir, shell: "/bin/bash" });
 };
 
