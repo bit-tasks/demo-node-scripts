d7683b54cd54af19e77067179228a251eac02eb6
Ashan Fernando <ashan256@gmail.com>
2023-10-30 13:54:35 +0530

    Update tag-export.js

--- scripts/tag-export.js
+++ scripts/tag-export.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 const run = (wsdir) => {
-  execSync('bit tag -m "CI" --build', { cwd: wsdir, shell: "/bin/bash" });
+  execSync('bit tag -m "CI" --build', { cwd: wsdir, shell: "/bin/bash" }); // add --persist flag for soft tagging workflow
   execSync("bit export", { cwd: wsdir, shell: "/bin/bash" });
 };
 
